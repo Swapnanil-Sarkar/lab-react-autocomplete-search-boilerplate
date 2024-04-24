d995a20669aef2d2687f786e3ff5b270496027ac
Swapnanil Sarkar <you@example.com>
2024-04-24 13:23:28 +0530

    Updated Files

---
+++ autoComplete/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import CountrySearch from "./CountrySearch";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Search Your Country</h1>
+      <CountrySearch />
+    </div>
+  );
+};
+
+export default App;
